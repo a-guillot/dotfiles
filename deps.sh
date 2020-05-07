55ee89909d585884a020db19d7df418447c7413d
Andreas Guillot <and.guillot@gmail.com>
2020-05-07 02:41:39 +0200

    add useful packages

--- deps.sh
+++ deps.sh
@@ -10,6 +10,21 @@ sudo apt install curl -y
 sudo apt install python -y
 sudo apt install neovim -y
 sudo apt install zsh -y
+sudo apt install texlive-full -y
+sudo apt install docker docker.io containerd docker-ce docker-ce-cli containerd.io -y
+sudo apt install borgbackup -y
+sudo apt install youtube-dl -y
+sudo apt install lifeograph -y
+sudo apt install vlc -y
+sudo add-apt-repository ppa:seafile/seafile-client -y
+sudo apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt install kubeadm -y
+sudo apt install seafile-gui seafile-cli -y
+sudo apt install lifeograph -y
+sudo apt install stretchly -y
+sudo apt install conda -y
+#echo /home/andreas/Seafile/'My Library'\
+#borg init --encryption=repokey Seafile/My\ Library
 
 # Useful python packages
 sudo apt install python3-matplotlib -y
@@ -17,3 +32,5 @@ sudo apt install python3-pandas -y
 sudo apt install python3-statsmodels -y
 sudo apt install python3-sklearn -y
 sudo apt install ipython3 -y
+
+sudo snap install discord --classic
