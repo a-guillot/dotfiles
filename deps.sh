6d76ef426630647fc405628255d5033114805fbf
Andreas Guillot <and.guillot@gmail.com>
2020-05-12 03:53:41 +0200

    added conditionals for big programs and borg init

--- deps.sh
+++ deps.sh
@@ -7,24 +7,10 @@ sudo apt install cmake -y
 sudo apt install git -y
 sudo apt install vim -y
 sudo apt install curl -y
-sudo apt install python -y
 sudo apt install neovim -y
 sudo apt install zsh -y
-sudo apt install texlive-full -y
 sudo apt install docker docker.io containerd docker-ce docker-ce-cli containerd.io -y
-sudo apt install borgbackup -y
-sudo apt install youtube-dl -y
-sudo apt install lifeograph -y
-sudo apt install vlc -y
-sudo add-apt-repository ppa:seafile/seafile-client -y
-sudo apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt install kubeadm -y
-sudo apt install seafile-gui seafile-cli -y
-sudo apt install lifeograph -y
-sudo apt install stretchly -y
 sudo apt install conda -y
-#echo /home/andreas/Seafile/'My Library'\
-#borg init --encryption=repokey Seafile/My\ Library
 
 # Useful python packages
 sudo apt install python3-matplotlib -y
@@ -33,4 +19,18 @@ sudo apt install python3-statsmodels -y
 sudo apt install python3-sklearn -y
 sudo apt install ipython3 -y
 
-sudo snap install discord --classic
+echo "Do you wish to install big programs?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) sudo apt install borgbackup -y; sudo snap install discord --classic; sudo apt install stretchly -y; sudo apt install vlc -y; sudo add-apt-repository ppa:seafile/seafile-client -y; sudo apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"; sudo apt install kubeadm -y; sudo apt install seafile-gui seafile-cli -y; sudo apt install texlive-full -y; sudo apt install youtube-dl -y; sudo apt install lifeograph -y; break;;
+        No ) break;;
+    esac
+done
+
+echo "Do you wish to inibitalize borg?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) borg init --encryption=repokey Seafile/My\ Library; break;;;
+        No ) break;;
+    esac
+done
